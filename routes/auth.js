9067b48d813dbd5d6e79a6ad49ad0d4c82a051d6
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-11-07 00:13:05 +0530

    add bcryptjs dependency and implement password hashing in signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
-
+const bcrypt = require("bcryptjs");
 const authRouter = express.Router();
 
 authRouter.post("/api/signup", async (req, res) => {
@@ -12,8 +12,14 @@ authRouter.post("/api/signup", async (req, res) => {
     if (emailUsed) {
       return res.status(400).json({ message: "Email is already in use" });
     } else {
+      //create a salt with cost factor 10
+      const salt = await bcrypt.genSalt(10);
+
+      //create a hashed password using the salt
+      const hashedPassword = await bcrypt.hash(password, salt);
+
       //return res.status(200).json({message:"Email is available"});
-      let user = new User({ fullname, username, email, password });
+      let user = new User({ fullname, username, email, password:hashedPassword });
       user = await user.save();
       res.json(user);
     }
